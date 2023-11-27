088ced2b8eb3d22d69dbe9d1e4a26cfa51b6741f
achitti2812 <achitti@ncsu.edu>
2023-11-27 12:03:51 -0500

    added logic in mood_event.py

---
+++ src/functionality/mood_events.py
@@ -0,0 +1,12 @@
+MOOD_EVENT_MAP = {
+    "happy": ["concert", "theme park visit", "comedy show", "group sports", "picnic"],
+    "stressed": ["spa day", "yoga session", "meditation class", "nature walk", "reading at a cafe"],
+    "sad": ["movie night", "art therapy session", "comfort food cooking class", "poetry reading", "visit to a pet shelter"],
+    "energetic": ["dance class", "hiking", "cycling tour", "martial arts class", "fitness boot camp"],
+    "relaxed": ["beach day", "jazz concert", "wine tasting event", "garden tour", "book club meeting"],
+    "adventurous": ["rock climbing", "scuba diving", "paragliding", "safari tour", "mystery escape room"],
+    "romantic": ["candlelit dinner", "sunset cruise", "ballroom dancing", "stargazing night", "couple's massage"],
+    "creative": ["painting workshop", "pottery class", "DIY craft session", "writing retreat", "photography walk"],
+    "curious": ["science museum visit", "historical landmark tour", "astronomy lecture", "technology expo", "archeological dig experience"],
+    "melancholic": ["classical music concert", "poetry writing workshop", "black and white film marathon", "museum visit", "solo nature walk"]
+}
