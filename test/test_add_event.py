fe9ead7d91af3ed5249d09baa07498a4bc7ce909
Deepak Sai Pendyala <76196490+deepaksaipendyala@users.noreply.github.com>
2024-11-02 18:59:10 -0400

    Update test_add_event.py

--- test/test_add_event.py
+++ test/test_add_event.py
@@ -20,8 +20,8 @@ from functionality.AddEvent import add_event
 # Configure fixtures for client and bot
 @pytest.fixture
 def client(event_loop):
-    c = discord.Client(loop=event_loop)
-    test.configure(c)
+    intents = discord.Intents.default()  # or customize the intents you need
+    c = discord.Client(loop=event_loop, intents=intents)
     return c
 
 @pytest.fixture
